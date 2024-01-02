86335d78366fb67057c1bd7068c426723c9738c2
Sandeep Reddy Bommineni <sandeepreddybommineni2605@gmail.com>
2024-01-02 16:51:10 +0530

    commit ffp1

--- FFP/ffp1.sql
+++ FFP/ffp1.sql
@@ -23,9 +23,11 @@ foreign key(store_id) references store(store_id),foreign key(member_id) referenc
 
 create table order_details(order_id int,item_name varchar(20),quantity int,price int,date date);
 
-create user dbffp_user;
+create user ffp_user;
 
-Grant ALL on FFP to dbffp_user;
+Grant ALL on FFP to ffp_user;
+
+create index idx_memberid on member(member_id);
 
 
 
