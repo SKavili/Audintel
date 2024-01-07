f8832fd46ebf5dee489b7c20db51d927b0a7edbc
vineeth <vineethsajjan7736@gmail.com>
2024-01-07 10:50:12 +0530

    trgger to update points

--- FFP/ffp.sql
+++ FFP/ffp.sql
@@ -28,3 +28,6 @@ create index idx_memberid on member(member_id);
 ALTER TABLE member modify COLUMN phno varchar(40);
 ALTER TABLE store modify COLUMN store_name varchar(40);
 ALTER TABLE order_details modify COLUMN item_name varchar(60);
+ALTER TABLE travel_info DROP COLUMN points;
+Create table points(member_id int,ticket_id int,points int);
+truncate table points;
